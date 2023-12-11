bb36c7c631a75d7a5a0ddffb5b8b12fbfc2fcabe
sanjeevWeb <kumarsanjeevdutta02@gmail.com>
2023-12-11 18:33:50 +0530

    included bcryptjs package to hash password in registe and compare password  in login

--- backend/controller/routeControllers.js
+++ backend/controller/routeControllers.js
@@ -1,63 +1,80 @@
 const pool = require("../db/database");
+const bcrypt = require("bcryptjs")
 
-const RegisterFunc = (req, res) => {
+const RegisterFunc = async (req, res) => {
     const { username, email, pass } = req.body;
     if (!username || !email || !pass) {
         return res.json({ errormsg: 'All fields are required' })
     }
 
-    const email_query = `SELECT COUNT(*) AS count FROM expenses WHERE email = ?`;
-    const db_query = `INSERT INTO expenses (name,email,pass) VALUES (?,?,?)`;
+    try {
+        //password hashing
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(pass, salt);
 
-    pool.query(email_query, [email], (err, result) => {
-        if (err) {
-            return res.json({ error: 'can not be verified' })
-        }
-        console.log(result)
-        const count = result[0].count;
-        const emailExists = count === 1;
+        const email_query = `SELECT COUNT(*) AS count FROM expenses WHERE email = ?`;
+        const db_query = `INSERT INTO expenses (name,email,pass) VALUES (?,?,?)`;
 
-        if(emailExists){
-            return res.json({ message: 'email already exists' })
-        }
-        pool.query(db_query, [username, email, pass], (err, result) => {
+        pool.query(email_query, [email], (err, result) => {
             if (err) {
-                return res.json({ error: 'sorry, something broke' });
+                return res.json({ error: 'can not be verified' })
             }
             console.log(result)
-            return res.json({ message: 'data inserted successfully' });
+            const count = result[0].count;
+            const emailExists = count === 1;
+
+            if (emailExists) {
+                return res.json({ message: 'email already exists' })
+            }
+            pool.query(db_query, [username, email, hashedPass], (err, result) => {
+                if (err) {
+                    return res.json({ error: 'sorry, something broke' });
+                }
+                console.log(result)
+                return res.json({ message: 'data inserted successfully' });
+            })
         })
-    })
+    }
+    catch (error) {
+        console.log(error)
+    }
 
 }
 
-const LoginFunc = (req,res) => {
+const LoginFunc = (req, res) => {
     const { email, passKey } = req.body
-    if(!email || !passKey){
-        return res.json({ message: 'please enter all fields'});
+    if (!email || !passKey) {
+        return res.json({ message: 'please enter all fields' });
     }
-    const db_query = `SELECT * FROM expenses WHERE email = ?`;
-    pool.query(db_query, [email], (err,result) => {
-        if(err){
-            return res.json({ error: 'something broke'});
-        }
-        console.log(result);
-        if(result.length === 0){
-            return res.json({message: 'user not found'})
-        }
-        if(result[0].email === email){
-            if(result[0].pass === passKey){
-                return res.json({ message: 'log in successfully'});
-            }
-            else{
-                return res.json({ message: 'User not authorized'})
-            }
-        }
-        else{
-            return res.json({ message: 'User not found'})
-        }
+    try {
+        const db_query = `SELECT * FROM expenses WHERE email = ?`;
 
-    })
+        pool.query(db_query, [email], async (err, result) => {
+            if (err) {
+                return res.json({ error: 'something broke' });
+            }
+            console.log(result);
+            if (result.length === 0) {
+                return res.json({ message: 'user not found' })
+            }
+            if (result[0].email === email) {
+                const isMatched = await bcrypt.compare(passKey, result[0].pass);
+                if (isMatched) {
+                    return res.json({ message: 'log in successfull' })
+                }
+                else {
+                    return res.json({ message: 'user not authorized' })
+                }
+            }
+            else {
+                return res.json({ message: 'User not found' })
+            }
+
+        })
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
 
 module.exports = {
--- forntend/script.js
+++ forntend/script.js
@@ -3,6 +3,28 @@ const username = document.querySelector('#username');
 const email = document.querySelector('#email');
 const pass = document.querySelector('#pass');
 
+//login form values
+const regEmail = document.querySelector('#regEmail');
+const password = document.querySelector('#password');
+const loginForm = document.querySelector('#loginForm');
+
+//register and login switch bittons
+const gotoRegister = document.querySelector('#gotoRegister');
+const gotoLogin = document.querySelector('#gotoLogin');
+
+gotoRegister.addEventListener('click', () => {
+    form.classList.remove('form_hidden');
+    loginForm.classList.add('form_hidden'); // after removing href, it is working fine ( with this code)
+    // loginForm.className = 'form_hidden';
+    gotoLogin.style.cursor = 'pointer'
+})
+
+gotoLogin.addEventListener('click', () => {
+    form.classList.add('form_hidden');
+    loginForm.classList.remove('form_hidden');
+    gotoRegister.style.cursor = 'pointer'
+})
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const obj = {
@@ -15,10 +37,28 @@ form.addEventListener('submit', (e) => {
         alert('all fields are required')
         return;
     }
-    axios.post('http://localhost:5000/api', obj )
+    axios.post('http://localhost:5000/api/signup', obj )
     .then(result => {
         console.log(result.data)
-        alert(result.data.username)
+        alert(result.data.message)
     })
     .catch(err => console.log(err))
+})
+
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = regEmail.value;
+    const passKey = password.value;
+    if(!email || !passKey){
+        alert('All fields are required');
+        return;
+    }
+    axios.post('http://localhost:5000/api/login', { email, passKey})
+    .then(result => {
+        console.log(result);
+        alert(result.data.message);
+    })
+    .catch(err => console.log(err))
+
 })
